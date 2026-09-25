ab98d0008464f4a19d15b9e2234f5d3a30e10496
Ricardo E. Melchor <rmelchor@reynera.com.mx>
2026-09-25 17:23:31 -0600

    Showing important notes only, client component solution

--- notes-app/app/notes/page.tsx
+++ notes-app/app/notes/page.tsx
@@ -1,19 +1,12 @@
-import Link from "next/link"
 import { getNotes } from "../services/notes"
+import NoteList from "./NoteList"
 
 const Notes = () => {
   const notes = getNotes()
   return (
     <div>
       <h2>Notes</h2>
-      <ul>
-        {notes.map(note => (
-          <li key={note.id}>
-            <Link href={`/notes/${note.id}`}>{note.content}</Link>{" "}
-            {note.important && <strong>(important)</strong>}
-          </li>
-        ))}
-      </ul>
+      <NoteList notes={notes} />
     </div>
   )
 }
